b30cbc9e57a2d8ab880f483f1c2c45f2e59176d7
Robert Baxter <robertbaxter@MacBook-Pro-443.fios-router.home>
2022-05-08 10:09:20 -0400

    ugh

--- packages/backend-service/index.test.ts
+++ packages/backend-service/index.test.ts
@@ -2,7 +2,7 @@ import { dateFormatService } from ".";
 
 describe("dateFormatService", () => {
   it("dateFormatService() should print the correct date", () => {
-    const result = dateFormatService(new Date("1991-05-08T00:00:00.000Z"));
+    const result = dateFormatService(new Date("2020-04-13T00:00:00.000+08:00"));
     expect(result).toMatchSnapshot();
   });
 });
