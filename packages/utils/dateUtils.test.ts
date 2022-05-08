e1c80e04f05ace13ca38d3f093316ec9506c7457
Robert Baxter <robertbaxter@Roberts-MBP.fios-router.home>
2022-05-07 21:15:33 -0400

    fix test

--- packages/utils/dateUtils.test.ts
+++ packages/utils/dateUtils.test.ts
@@ -2,7 +2,7 @@ import { addDays } from "./dateUtils";
 
 describe("dateUtils", () => {
   it("addDays() should add days", () => {
-    const result = addDays(new Date("05/05/1991"), 3);
+    const result = addDays(new Date("05/05/1991").setHours(0, 0, 0, 0), 3);
     expect(result).toMatchSnapshot();
   });
 });
